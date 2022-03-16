e065dcc43aa0311a897327dc265dcc4b1160f539
Sofiane Hachemaoui <sofiane068@hotmail.fr>
2022-03-16 14:58:04 +0100

    link css et bootstrap

---
+++ src/Controller/PageAcceuilController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PageAcceuilController extends AbstractController
+{
+    #[Route('/page/acceuil', name: 'app_page_acceuil')]
+    public function index(): Response
+    {
+        return $this->render('page_acceuil/index.html.twig', [
+            'controller_name' => 'PageAcceuilController',
+        ]);
+    }
+}
