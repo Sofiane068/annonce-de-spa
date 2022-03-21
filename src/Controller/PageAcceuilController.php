e1036353c50070c0eead6249eee287f02b9d4977
Hakim Hamida <h.hamidawork@gmail.com>
2022-03-21 14:51:10 +0100

    ajoutPageInscritptionContact

--- src/Controller/PageAcceuilController.php
+++ src/Controller/PageAcceuilController.php
@@ -8,8 +8,8 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PageAcceuilController extends AbstractController
 {
-    #[Route('/page/acceuil', name: 'app_page_acceuil')]
-    public function index(): Response
+    #[Route('/', name: 'app_page_acceuil')]
+    public function index(string $name = null): Response
     {
         return $this->render('page_acceuil/index.html.twig', [
             'controller_name' => 'PageAcceuilController',
---
+++ src/Entity/ContactForm.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ContactFormRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: ContactFormRepository::class)]
+class ContactForm
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\Email()]
+    private $email;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\Length(min:20)]
+    private $message;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+}
