14c40b7284d31e311dc7229c76188a11a0b2df20
Sofiane Hachemaoui <sofiane068@hotmail.fr>
2022-03-22 09:49:23 +0100

    creation de connexion type

---
+++ src/Form/ConnexionType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Utilisateur;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ConnexionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('mail', EmailType::class)
+            ->add('motDePasse', PasswordType::class)
+            ->add("submit", SubmitType::class, [
+            'label' => "Envoyer",
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Utilisateur::class,
+        ]);
+    }
+}
